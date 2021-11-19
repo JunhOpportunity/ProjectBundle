665ee27ae744f08cb79777385de873492e0ca588
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-20 01:26:49 +0900

    New project Frame Complete

---
+++ Moon_Phase_For_Observation/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.blackVer');
+// btn.addEventListener('click', () => {
+//     body.style.Color = 'black';
+// })
+function bgchange () {
+    document.body.style.backgroundColor = "tomato";
+}
+btn.addEventListener("click", bgchange);
